79f9f54516dbaf112403aa34afed2c7e661336df
DomenicG46 <DomenicG46@outlook.com>
2024-07-29 16:10:13 -0500

    Completed Assignment

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # customer_banking
 Module 3 Challenge
+
+Completed this project with alot more confidence and knowledge. 
+The resources have been tremendous in helping me get through the course. I am
+very appreciative. I worked on a Slack Huddle w teamates, Microsoft Teams and
+scheduled a Tutor (Brandon) to help me review. 
+Boom! All complete. I hope.
--- customer_banking.py
+++ customer_banking.py
@@ -1,5 +1,7 @@
 # Import the create_cd_account and create_savings_account functions
 # ADD YOUR CODE HERE
+from cd_account import create_cd_account
+from savings_account import create_savings_account
 
 # Define the main function
 def main():
@@ -9,21 +11,46 @@ def main():
     """
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     # ADD YOUR CODE HERE
+    savings_balance = float(input('What is your Savings Account Balance?'))
+    savings_interest = float(input('What is the APR for Savings Account?'))
+    savings_maturity = int(input('What is the Length of Term?'))                     
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    # Display the savings account data.
+    print('Here are the details of the savings account.')
+    print("The balance is: $", format(savings_balance, ',.2f'))
+    print("APR Interest Rate is: ", format(savings_interest, ',.2f'),"%")
+    print(f"Length of CD is: {savings_maturity}months")
+    print("Savings Interest Earned would be: $ ", format(interest_earned, ',.2f'))
+    print(f"Your projected Savings balance will be: ${updated_savings_balance}")
+
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
+    cd_balance = float(input('What is your CD Account Balance?'))
+    cd_interest = float(input('What is the APR for CD Account?'))
+    cd_maturity = int(input('What is the Length of CD Term?'))
 
     # Call the create_cd_account function and pass the variables from the user.
-    updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
+    updated_cd_balance, interest_cd_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
+    print('Here are the details of the projected CD account.')
+    print("The balance is: $", format(cd_balance, ',.2f'))
+    print("APR Interest Rate is: ", format(cd_interest, ',.2f'),"%")
+    print(f"Length of CD is: {cd_maturity}months")
+    print("CD Interest Earned would be: $ ", format(interest_cd_earned, ',.2f'))
+    print(f"Your projected CD balance will be: ${updated_cd_balance}")
+    
 
 if __name__ == "__main__":
-    # Call the main function.
+   
+# Call the main function.
+    main ()
+
+
